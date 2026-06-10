c037609b8b9e585a76610d89da1492a03a53524c
Innei <i@innei.in>
2026-06-10 17:43:13 +0800

    💄 style(chat-input): fix control bar height jump when TokenTag appears (#15643)

--- src/features/ChatInput/ControlBar/index.tsx
+++ src/features/ChatInput/ControlBar/index.tsx
@@ -14,6 +14,7 @@ import WorkspaceControls from './WorkspaceControls';
 
 const styles = createStaticStyles(({ css }) => ({
   bar: css`
+    height: 28px;
     padding-block: 0;
     padding-inline: 4px;
   `,
