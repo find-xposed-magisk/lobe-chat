f5e91e0e7e938c79595993591a5ecabf64f413fa
Innei <i@innei.in>
2026-06-30 00:38:09 +0800

    ♻️ refactor(settings): remove subscription iframe wrapper (#16448)

--- src/business/client/BusinessSettingPages/Plans.tsx
+++ src/business/client/BusinessSettingPages/Plans.tsx
@@ -1,14 +1,3 @@
-'use client';
+const Plans = () => null;
 
-import { isDesktop } from '@lobechat/const';
-import { memo } from 'react';
-
-import { SubscriptionIframeWrapper } from './SubscriptionIframeWrapper';
-
-const Plans = memo(() => {
-  if (!isDesktop) return null;
-  return <SubscriptionIframeWrapper page="plans" />;
-});
-
-Plans.displayName = 'Plans';
 export default Plans;
--- src/business/client/BusinessSettingPages/Usage.tsx
+++ src/business/client/BusinessSettingPages/Usage.tsx
@@ -1,14 +1,3 @@
-'use client';
+const Usage = () => null;
 
-import { isDesktop } from '@lobechat/const';
-import { memo } from 'react';
-
-import { SubscriptionIframeWrapper } from './SubscriptionIframeWrapper';
-
-const Usage = memo(() => {
-  if (!isDesktop) return null;
-  return <SubscriptionIframeWrapper page="usage" />;
-});
-
-Usage.displayName = 'Usage';
 export default Usage;
